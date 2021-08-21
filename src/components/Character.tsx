f4cc133ccbf78bed35c03b95a372acb9082ac5a0
Salman Salari <salmansalary@gmail.com>
2021-08-21 14:08:17 +0800

    seperated char state

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -6,13 +6,13 @@ import { randomInt } from '../utils';
 type CharProps = {
     onItemClick:Function,
     forceStop: boolean,
-    id: number,
-    charStates: any
+    isActive?: boolean,
+    inActivate: Function
 }
 
 const charPoints = [-1,-2,-3,-1,-2,-3,-1,5];
 
-const Character = ({onItemClick, id, charStates , forceStop }: CharProps)=>{
+const Character = ({onItemClick, isActive, inActivate, forceStop }: CharProps)=>{
 
     const [ char, setChar ] = useState({ className : '', point: 0 });
 
@@ -22,38 +22,30 @@ const Character = ({onItemClick, id, charStates , forceStop }: CharProps)=>{
 
         let poofTimer, lock;
 
-        const checkingTime = randomInt(4,7) * 100;
+        if(isActive && !lock){
 
-        const gClock = setInterval(() => {
+            lock = true;
+            //The probebility of Fox Non-Fox 50% - 50%
+            const nomination = Math.random() >= 0.5  ? 7  :  randomInt(0,6);
             
-            if(charStates[id] && !lock){
+            setChar({
+                className: `char${nomination}`,
+                point: charPoints[nomination]
+            })
 
-                lock = true;
-                //The probebility of Fox Non-Fox 50% - 50%
-                const nomination = Math.random() >= 0.5  ? 7  :  randomInt(0,6);
-                
-                setChar({
-                    className: `char${nomination}`,
-                    point: charPoints[nomination]
-                })
+            //remove the active state for this hole after 2 second for next random nomination
+            poofTimer = setTimeout(function(){
+                if(isActive) inActivate();
+                lock = false;
+                clearTimeout(poofTimer);
+            },1100)
 
-                //remove the active state for this hole after 2 second for next random nomination
-                poofTimer = setTimeout(function(){
-                    charStates[id] = false;
-                    setChar({ className: '', point: 0 });
-                    lock = false;
-                    clearTimeout(poofTimer);
-                },1500)
+        } else setChar({ className: '', point: 0 })
 
-            }
-        }, checkingTime) as any;
 
-        return () => {
-            clearInterval(gClock)
-            clearTimeout(poofTimer);
-        }
+        return () => clearTimeout(poofTimer);
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [forceStop]);
+    }, [forceStop, isActive]);
 
     return <div className='box'>
             <div className="charContainer">
@@ -64,7 +56,7 @@ const Character = ({onItemClick, id, charStates , forceStop }: CharProps)=>{
                         ev.stopPropagation();
                         onItemClick(char?.point);
                         
-                        if(charStates[id] && !forceStop) setChar({ className: '', point: 0 })             
+                        if(isActive) inActivate()             
                         
                     }}
                 />
@@ -73,6 +65,6 @@ const Character = ({onItemClick, id, charStates , forceStop }: CharProps)=>{
         </div>
 }
 
-const propsAreSameIf = (pre:CharProps,cur:CharProps) => pre.forceStop === cur.forceStop
+const propsAreSameIf = (pre:CharProps,cur:CharProps) => pre.forceStop === cur.forceStop && pre.isActive === cur.isActive
 
 export default React.memo(Character,propsAreSameIf);
--- src/components/PlayGround.tsx
+++ src/components/PlayGround.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect } from 'react';
+import React, {useEffect, useState, useCallback } from 'react';
 import Character from './Character';
 import './style.scss'
 import { randomInt } from '../utils';
@@ -6,23 +6,66 @@ import { randomInt } from '../utils';
 type Props = {
     onItemClick:Function,
     forceStop: boolean,
-    initialState?: boolean[]// for testing
-    charStates: any;
+    initialState?: boolean[],// for testing
+    charStates: any
 
 }
 
 type CharProps = {
-    id: number;
     onItemClick:Function,
     forceStop: boolean,
-    charStates: any;
+    isActive?: boolean,
+    inActivate: Function
 }
 
-const PlayGround = ({onItemClick, forceStop, charStates}:Props) => {
+const PlayGround = ({onItemClick, forceStop,charStates}:Props) => {
+
+    const [ active_0, setActive_0 ] = useState(charStates[0]);
+    const [ active_1, setActive_1 ] = useState(charStates[1]);
+    const [ active_2, setActive_2 ] = useState(charStates[2]);
+    const [ active_3, setActive_3 ] = useState(charStates[3]);
+    const [ active_4, setActive_4 ] = useState(charStates[4]);
+    const [ active_5, setActive_5 ] = useState(charStates[5]);
+    const [ active_6, setActive_6 ] = useState(charStates[6]);
+    const [ active_7, setActive_7 ] = useState(charStates[7]);
+    const [ active_8, setActive_8 ] = useState(charStates[8]);
+
+    const setCharState = useCallback(function(id,st){
+        switch(id) {
+            case 0:
+                setActive_0(st);
+                break;
+            case 1:
+                setActive_1(st);
+                break;
+            case 2:
+                setActive_2(st);
+                break;
+            case 3:
+                setActive_3(st);
+                break;
+            case 4:
+                setActive_4(st);
+                break;
+            case 5:
+                setActive_5(st);
+                break;
+            case 6:
+                setActive_6(st);
+                break;
+            case 7:
+                setActive_7(st);
+                break;
+            case 8:
+                setActive_8(st);
+                break;
+            default:
+          }
+    },[]);
 
     useEffect(() => {
 
-        const gClock = setInterval(() => charStates[randomInt(0,8)] = true, 500) as any;
+        const gClock = setInterval(() => setCharState(randomInt(0,8),true), 500) as any;
         return () => clearInterval(gClock)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
@@ -33,27 +76,26 @@ const PlayGround = ({onItemClick, forceStop, charStates}:Props) => {
         return {
             forceStop,
             onItemClick,
-            charStates,
-            id
+            inActivate: setCharState.bind({},id,false)
         } as CharProps;
 
     }
 
     return <div className="playGround">
         <div className="row">
-            <Character {...getCharProps(0)} />
-            <Character {...getCharProps(1)} />
-            <Character {...getCharProps(2)} />
+            <Character  isActive = {active_0} {...getCharProps(0)} />
+            <Character  isActive = {active_1} {...getCharProps(1)} />
+            <Character  isActive = {active_2} {...getCharProps(2)} />
         </div>
         <div className="row">
-            <Character {...getCharProps(3)} />
-            <Character {...getCharProps(4)} />
-            <Character {...getCharProps(5)} />
+            <Character  isActive = {active_3} {...getCharProps(3)} />
+            <Character  isActive = {active_4} {...getCharProps(4)} />
+            <Character  isActive = {active_5} {...getCharProps(5)} />
         </div>
         <div className="row">
-            <Character {...getCharProps(6)} />
-            <Character {...getCharProps(7)} />
-            <Character {...getCharProps(8)} />
+            <Character  isActive = {active_6} {...getCharProps(6)} />
+            <Character  isActive = {active_7} {...getCharProps(7)} />
+            <Character  isActive = {active_8} {...getCharProps(8)} />
         </div>
     </div>
 }
